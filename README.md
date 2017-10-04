ae8363d081061a043f59113c362ed6ff05bddb42
mfalcier <marcofalcier@gmail.com>
2017-10-04 10:54:55 +0200

    :sparkles: Added config-service

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # reservation-service
+
+Example from: https://www.youtube.com/watch?v=5q8B6lYhFvE
+
+Refresh: curl -d{} http://localhost:8000/refresh
--- src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example.reservationservice;
 
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -75,5 +80,22 @@ class SampleDataCLR implements CommandLineRunner {
 				.forEach(name -> this.reservationRepository.save(new Reservation(name)));
 		reservationRepository.findAll().forEach(System.out::println);
 	}
+}
+
+@RestController
+@RefreshScope
+class MessageRestController {
+    private final String value;
+
+    @Autowired
+    public MessageRestController(@Value("${message}") String value) {
+        this.value = value;
+
+    }
+
+    @GetMapping("/message")
+    String read() {
+        return this.value;
+    }
 }
 
